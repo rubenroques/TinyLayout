ab6c24467a569c0b7639b60defe9a31ff40b0068
Ruben Roques <rubenroques.nhk@gmail.com>
2015-10-03 16:55:49 +0100

    Migrated project to support Carthage

---
+++ TinyLayout/TinyLayout.h
@@ -0,0 +1,19 @@
+//
+//  TinyLayout.h
+//  TinyLayout
+//
+//  Created by Ruben Roques on 03/10/15.
+//  Copyright © 2015 Ruben Roques. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TinyLayout.
+FOUNDATION_EXPORT double TinyLayoutVersionNumber;
+
+//! Project version string for TinyLayout.
+FOUNDATION_EXPORT const unsigned char TinyLayoutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TinyLayout/PublicHeader.h>
+
+
